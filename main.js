e4f629fc37d5a20288b308f13124268a464d5421
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-06-29 23:15:37 +0200

    Add minor optimizations

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-JSONstat(
-  'http://data.ssb.no/api/v0/dataset/1106.json?lang=no',
-  function(){
-    var html=JSONstatUtils.datalist(
-      this,
-      {
-        counter: true,
-        tblclass: "table table-striped table-bordered table-condensed"
-      }
-    );
-    $('#debug').html(html);
-    $('#debug').find('table').attr("id", "table");
+$(document).ready(function() {
+/*
+    MANGLER:
+    - Ytelsesforbedring
+        - CDN
+        - Lokale kopier av bg, js, etc.
+        - Caching
+    - Filtrering (på variabler/kategorier i datacube)
+    - Generalisering (brukbar med hvilket som helst datasett)
+    - Universell utforming
+ */
+    if (typeof(Storage) !== "undefined") {
+        // Code for localStorage/sessionStorage.
+        console.log('Storage available.')
+    } else {
+        alert('This site needs LocalStorage to work properly.\nPlease enable it or switch to a modern browser.')
+    }
 
-    // Fix bad behavior from JSONstat library
-    $('<thead></thead>').prependTo('#table').append($('#table tbody tr:first'));
 
-    //new Tablesort(document.getElementById('table'));
-    $(document).ready(function() {
+    JSONstat(
+      'http://data.ssb.no/api/v0/dataset/1108.json?lang=no',
+      function(){
+        var html=JSONstatUtils.datalist(
+          this,
+          {
+            counter: true,
+            tblclass: "table table-striped table-bordered table-condensed"
+          }
+        );
+        $('#debug').html(html);
+        $('#debug').find('table').attr("id", "table");
+
+        // Fix bad behavior from JSONstat library
+        $('<thead></thead>').prependTo('#table').append($('#table tbody tr:first'));
         $('#table').DataTable();
-    } );
+    });
   }
 );
-
-/*
-        MANGLER:
-        - Caching
-        - Filtrering (på variabler/kategorier)
-        - Generalisering (brukbar med hvilket som helst datasett)
-        - Ikoner (trekanter) i thead, indikasjon på nåværende sortering
-        - Ytelsesforbedring
-        - Universell utforming
- */
